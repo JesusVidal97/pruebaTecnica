80827d87d9fc6a0ccfc3b4bc674d9932dc3ab314
Jesús Vidal <36632584+JesusVidal97@users.noreply.github.com>
2025-04-04 19:32:07 +0200

    Add files via upload
    feat: commit inicial del proyecto

---
+++ src/main/java/com/jesus/prueba_tecnica/PruebaTecnicaApplication.java
@@ -0,0 +1,14 @@
+package com.jesus.prueba_tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PruebaTecnicaApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaApplication.class, args);
+	}
+
+}
